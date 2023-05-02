358ffde75cf5fbbd2287e1e937504ad81fb85a39
Evan Hesketh <evanhesketh@yahoo.ca>
2023-05-02 15:33:01 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, flash, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import Unauthorized
+
+# from forms import UserAddForm, LoginForm, MessageForm, CsrfForm, UserUpdateForm
+# from models import db, connect_db, User, Message
+
+load_dotenv()
+
+CURR_USER_KEY = "curr_user"
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+# toolbar = DebugToolbarExtension(app)
+
+# connect_db(app)
