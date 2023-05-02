4cbd681613fdc5e4af4c3c314f1a856b7c89f836
Kevin Deakers <kmdeakers@mac.com>
2023-05-02 14:13:17 -0700

    start build post route for add-photo

--- app.py
+++ app.py
@@ -23,3 +23,10 @@ app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 # toolbar = DebugToolbarExtension(app)
 
 # connect_db(app)
+
+
+@app.post('/add-photo')
+def add_photo():
+    """ Add photo data to database and upload to AWS"""
+
+    
